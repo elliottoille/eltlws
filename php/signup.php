90b7f5a42232ab75a7775ae77dc58427dad192c6
Elliot Lewis <elliot@eltl.ws>
2021-08-15 22:12:23 +0100

    a0010

--- php/signup.php
+++ php/signup.php
@@ -8,30 +8,27 @@
         $password = $_POST["password"];
         $confirmPassword = $_POST["confirmPassword"];
 
-        $sql = "Select * from users where username='$username'";
+        $sql = "SELECT * FROM users WHERE username='$username'";
 
         $result = mysqli_query($conn, $sql);
 
         $num = mysqli_num_rows($result);
 
         if ($num == 0) {
+            echo "no other users with this username in table<br>";
             if ($password == $confirmPassword) {
+                echo "entered passwords do match<br>";
                 $hash = password_hash($password, PASSWORD_DEFAULT);
 
                 $sql = "INSERT INTO `users` (`username`, `password`) VALUES ('$username', '$hash')";
 
                 $result = mysqli_query($conn, $sql);
-
-                if ($result) {
-                    $displayAlert = true;
             } else {
-                echo "passwords dont match";
+                echo "passwords do not match";
             }
         }
         if ($num > 0) {
             echo "username not available";
         }
     }
-    }
-
 ?>
