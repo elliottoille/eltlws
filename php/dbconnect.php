72b7b59c2ed6033a2b05a84d00309ebc1603520f
Elliot Lewis <elliot@eltl.ws>
2021-08-15 22:31:39 +0100

    a0010.6

--- php/dbconnect.php
+++ php/dbconnect.php
@@ -8,5 +8,7 @@
     $conn = mysqli_connect($servername, $username, $password, $database);
     if(!$conn) {
         die("Error: ". mysqli_connect_error());
+    } else {
+        echo "connection succeeded<br>";
     }
 ?>
