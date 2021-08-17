3bcc41d26cfa62cbade658df8ec669c397a16652
Elliot Lewis <elliot@eltl.ws>
2021-08-17 21:37:53 +0100

    updated sql in login.php

--- php/login.php
+++ php/login.php
@@ -5,7 +5,7 @@
         $username = $_POST["username"];
         $password = $_POST["password"];
         
-        $sql = "select 'password' where 'username'='$username' from users";
+        $sql = "select 'password' from users where username='$username';";
 
         $result = mysqli_query($conn, $sql);
 
