f02b0c41e5d3d6778a1870c9e94146f063ad56a0
Elliot Lewis <elliot@eltl.ws>
2021-09-13 20:15:19 +0100

    Added comments to some php documents

--- php/dbconnect.php
+++ php/dbconnect.php
@@ -1,12 +1,12 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "xe5Y_U),xOrdw{Ku#iXD";
+    $servername = "localhost"; # Name of the server
+    $username = "root"; # Username to access database
+    $password = "xe5Y_U),xOrdw{Ku#iXD"; # Password of user 'root' to access database
 
-    $database = "eltlws";
+    $database = "eltlws"; # The name of the database on the server
 
-    $conn = mysqli_connect($servername, $username, $password, $database);
-    if(!$conn) {
-        die("Error: ". mysqli_connect_error());
+    $conn = mysqli_connect($servername, $username, $password, $database); # Create a connection to the database using these variables
+    if(!$conn) { # If the connection fails then
+        die("Error: ". mysqli_connect_error()); # Quit and report the error to the terminal
     }
 ?>
--- php/signup.php
+++ php/signup.php
@@ -1,28 +1,27 @@
 <?php
-    $displayAlert = false;
-    if($_SERVER["REQUEST_METHOD"] == "POST") {
-        include 'dbconnect.php';
+    if ($_SERVER["REQUEST_METHOD"] == "POST") { # If the server recieves a POST request then
+        include 'dbconnect.php'; # Include code from dbconnect.php in this document
 
-        $username = $_POST["username"];
-        $password = $_POST["password"];
-        $confirmPassword = $_POST["confirmPassword"];
+        $username = $_POST["username"]; # Sets username equal to the username passed by the POST method
+        $password = $_POST["password"]; # Sets password equal to the password passed by the POST method
+        $confirmPassword = $_POST["confirmPassword"]; # Sets confirmPassword to the confirm password passed by the POST method
 
-        $sql = "select * from users where username='$username';";
+        $sql = "SELECT * FROM `users` WHERE username='$username';"; # Create an SQL statement that fetches all the data from the database where the username equals the entered username
 
-        $result = mysqli_query($conn, $sql);
+        $result = mysqli_query($conn, $sql); # Query the database with the previous SQL statement
 
-        $num = mysqli_num_rows($result);
+        $num = mysqli_num_rows($result); # Store the amount of rows fetched from the previous SQL query
 
-        if ($num == 0) {
-            if ($password == $confirmPassword) {
-                $hash = password_hash($password, PASSWORD_DEFAULT);
-                $sql = "INSERT INTO `users` ( `username`, `password`) VALUES ('$username', '$hash')";
-                $result = mysqli_query($conn, $sql);
+        if ($num == 0) { # If the amount of rows returned is 0 (username doesn't already exist) then
+            if ($password == $confirmPassword) { # If both the entered passwords match each other then
+                $hash = password_hash($password, PASSWORD_DEFAULT); # Set hash equal to the entered password but hashed
+                $sql = "INSERT INTO `users` ( `username`, `password`) VALUES ('$username', '$hash');"; # Set the SQL statement to insert the entered users details into the database (stores the hashed password)
+                $result = mysqli_query($conn, $sql); # Query the database with the SQL statement
             } else {
-                echo "passwords do not match";
+                echo "passwords do not match"; # This will display on the webpage if both passwords do not match
             }
         } else {
-            echo "username not available";
+            echo "username not available"; # This will display on the webpage if something is returned in the initial query
         }
     }
 ?>
