215bb944b2afe36604af07beceb5c248b42bd51e
Elliot Lewis <elliot@eltl.ws>
2021-09-13 20:47:29 +0100

    Updated login.php comments

--- php/login.php
+++ php/login.php
@@ -1,16 +1,16 @@
 <?php
-    if($_SERVER["REQUEST_METHOD"] == "POST") {
-        include 'dbconnect.php';
+    if($_SERVER["REQUEST_METHOD"] == "POST") { # If the server recieves a POST request then
+        include 'dbconnect.php'; # Include code from dbconnect.php in this document
 
-        $username = $_POST["username"];
-        $password = $_POST["password"];
+        $username = $_POST["username"]; # Set the username equal to the username passed by the POST method
+        $password = $_POST["password"]; # Set the password equal to the password passed by the POST method
         
-        $sql = "select 'password' from users where username='$username';";
+        $sql = "SELECT `password` FROM `users` WHERE username=`$username`;"; # Create an SQL statement that fetches the password from the database where the username matches the entered username
 
-        $result = mysqli_query($conn, $sql);
+        $result = mysqli_query($conn, $sql); # Query the database with the previous SQL statement
 
-        if (password_verify($password, $result)) {
-            echo "passwords match";
+        if (password_verify($password, $result)) { # If the given password matches the password from the database (checks hashes of the passwords) then
+            echo "passwords match"; # Display that the entered passwords match on the webpage
         }
     }
 ?>
