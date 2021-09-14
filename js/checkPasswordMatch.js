3de2d6c41497bb9569e938171447875fc4f380ee
Elliot Lewis <elliot@eltl.ws>
2021-09-14 20:52:48 +0100

    Updated JS comments

--- js/checkPasswordMatch.js
+++ js/checkPasswordMatch.js
@@ -1,13 +1,13 @@
 function checkPasswordMatch() {
-    var password = document.getElementById('password');
-    var confirmPassword = document.getElementById('confirmPassword');
-    var alert = document.getElementById('passwordMatchAlert')
+    var password = document.getElementById('password'); // Set the password variable to the data inside the element with ID password
+    var confirmPassword = document.getElementById('confirmPassword'); // Set the confirmPassword variable to the data inside the element with ID confirmPassword
+    var alert = document.getElementById('passwordMatchAlert') // Set the alert variable to the element with ID passwordMatchAlert
 
-    if (password.value == confirmPassword.value) {
-        alert.style.color = '#00ff00';
-        alert.textContent = 'passwords match';
+    if (password.value == confirmPassword.value) { // If the two values match then
+        alert.style.color = '#00ff00'; // Set the alert element's color to green
+        alert.textContent = 'passwords match'; // Set the alert element's text to "passwords match"
     } else {
-        alert.style.color = '#ff0000';
-        alert.textContent = 'passwords do not match';
+        alert.style.color = '#ff0000'; // Set the alert element's color to red
+        alert.textContent = 'passwords do not match'; // Set the alert element's text to "passwords do not match"
     }
 }
